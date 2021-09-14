e16e8ea2ed0ab6d49f8ada25b0a9f71edb49a9d6
Marco Lin <pohanlin0605@gmail.com>
2021-09-14 09:26:05 +0800

    feat: add new function to search movie by movie's type

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
           v-model="inputText"
           type="text"
           class="home__input"
+          placeholder="please enter your keyword"
         >
         <button
           type="submit"
@@ -18,6 +19,56 @@
           Search
         </button>
       </form>
+      <div class="home__checkbox__wrapper">
+        <div class="home__checkbox__item">
+          <input
+            id="home__checkbox__movie"
+            type="radio"
+            name="home__checkbox"
+            class="home__checkbox"
+            value="movie"
+          >
+          <label
+            for="home__checkbox__movie"
+            class="home__label"
+          >
+            Movie
+            <span class="home__inline__checkbox" />
+          </label>
+        </div>
+        <div class="home__checkbox__item">
+          <input
+            id="home__checkbox__game"
+            type="radio"
+            name="home__checkbox"
+            class="home__checkbox"
+            value="game"
+          >
+          <label
+            for="home__checkbox__game"
+            class="home__label"
+          >
+            Game
+            <span class="home__inline__checkbox" />
+          </label>
+        </div>
+        <div class="home__checkbox__item">
+          <input
+            id="home__checkbox__series"
+            type="radio"
+            name="home__checkbox"
+            class="home__checkbox"
+            value="series"
+          >
+          <label
+            for="home__checkbox__series"
+            class="home__label"
+          >
+            Series
+            <span class="home__inline__checkbox" />
+          </label>
+        </div>
+      </div>
     </div>
     <Spinner v-if="isLoading" />
     <Table
@@ -58,9 +109,23 @@ export default {
   methods: {
     async searching () {
       this.$store.commit('nowIsLoading')
-      const { data } = await apiHelper.get(`${this.inputText}`)
-      this.movies = data.Search
-      this.$store.commit('searchingResults', Number(data.totalResults))
+      const checkboxs = document.querySelectorAll('.home__checkbox')
+      let typeValue = ''
+      checkboxs.forEach(checkbox => {
+        if (checkbox.checked) {
+          typeValue = checkbox.value
+        }
+      })
+      if (typeValue === '') {
+        const { data } = await apiHelper.get(`${this.inputText}`)
+        this.movies = data.Search
+        this.$store.commit('searchingResults', Number(data.totalResults))
+      }
+      if (typeValue !== '') {
+        const { data } = await apiHelper.get(`${this.inputText}&type=${typeValue}`)
+        this.movies = data.Search
+        this.$store.commit('searchingResults', Number(data.totalResults))
+      }
       this.$store.commit('getInput', this.inputText)
       this.$store.commit('nowIsLoading')
       this.inputText = ''
@@ -95,22 +160,58 @@ export default {
       letter-spacing: 0.5px;
       font-family: 'Chakra Petch', sans-serif;
       &:focus {
-        border: 1px solid #aecfff;
+        border: 1px solid $border-blue;
         box-shadow: none;
       }
     }
     &__button {
-      color: #118eee;
-      border: 1px solid #aecfff;
+      color: $header-text-blue;
+      border: 1px solid $border-blue;
       border-radius: 5px;
       margin-left: 15px;
       font-family: 'Chakra Petch', sans-serif;
       font-size: 1.1rem;
       &:hover {
-        background-color: #118eee;
+        background-color: $header-text-blue;
         color: #fff;
         border-color: transparent;
       }
+    }
+    &__checkbox__wrapper {
+      width: 90%;
+      max-width: 400px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 20px;
+    }
+    &__checlbox__item {
+      position: relative;
+    }
+    &__label {
+      @include text-style (1rem, 400, $header-text-blue);
+    }
+    &__checkbox {
+      display: none;
+    }
+    &__inline__checkbox {
+      @include checkbox-style (18px, 18px, 50%, relative, inline-block);
+      border: 1px solid $border-blue;
+      top: 2px;
+      &::after {
+        content: '';
+        @include checkbox-style (11px, 11px, 50%, relative, block);
+        background-color: $header-text-blue;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        opacity: 0;
+        transition: 0.6s ease-in;
+      }
+    }
+    &__checkbox:checked ~ &__label &__inline__checkbox::after {
+      opacity: 1;
     }
   }
 </style>
